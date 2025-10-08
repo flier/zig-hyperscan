e467278ab742bfb2b72989808117ed24315bc385
Flier Lu <flier.lu@gmail.com>
2025-10-08 21:54:49 +0800

    Enhance Zig Hyperscan library with MatchEvent and MatchEventHandler support. Updated scan functions to utilize event callbacks and added MatchContext for handling match events. Refactored simplegrep to demonstrate new event handling capabilities during pattern scanning.

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,3 @@
-const std = @import("std");
-
 const common = @import("common.zig");
 const compile = @import("compile.zig");
 const runtime = @import("runtime.zig");
@@ -7,6 +5,10 @@ const runtime = @import("runtime.zig");
 pub const Database = @import("database.zig");
 
 pub const Error = common.Error;
+pub const MatchAction = runtime.MatchAction;
+pub const MatchEvent = runtime.MatchEvent;
+pub const MatchEventHandler = runtime.MatchEventHandler;
+pub const MatchStartOffsetPostHorizon = runtime.MatchStartOffsetPastHorizon;
 pub const Platform = compile.Platform;
 pub const Scratch = runtime.Scratch;
 
--- src/simplegrep.zig
+++ src/simplegrep.zig
@@ -21,13 +21,6 @@ pub fn main() !void {
     const pattern = args[1];
     const input_file = args[2];
 
-    const db = try hs.Database.compile(pattern, .{});
-
-    const db_info = try db.info(allocator);
-    defer allocator.free(db_info);
-
-    std.log.debug("Hyperscan {s} Database size: {d} bytes", .{ db_info, try db.size() });
-
     var f = try std.fs.cwd().openFile(input_file, .{});
     defer f.close();
 
@@ -36,12 +29,41 @@ pub fn main() !void {
 
     std.log.debug("Data size: {d} bytes", .{data.len});
 
+    const db = try hs.Database.compile(pattern, .{
+        .flags = .SomLeftmost,
+    });
+
+    const db_info = try db.info(allocator);
+    defer allocator.free(db_info);
+
+    std.log.debug("Hyperscan {s} Database size: {d} bytes", .{ db_info, try db.size() });
+
     const scratch = try db.alloc_scratch();
     defer scratch.deinit();
 
     std.log.debug("Scratch size: {d} bytes", .{try scratch.size()});
 
+    const ctx = Context{
+        .patterns = &[_][]const u8{pattern},
+        .data = data,
+    };
+
     try db.scan_block(data, .{
         .scratch = scratch,
+        .onEvent = onEvent,
+        .context = @constCast(&ctx),
     });
+}
+
+const Context = struct {
+    patterns: []const []const u8,
+    data: []const u8,
+};
+
+fn onEvent(evt: hs.MatchEvent) hs.MatchAction {
+    const ctx: *Context = @ptrCast(@alignCast(evt.context));
+
+    std.log.info("Match for pattern #{} `{s}` at offset {}..{}: {s}", .{ evt.id, ctx.patterns[evt.id], evt.from, evt.to, ctx.data[evt.from..evt.to] });
+
+    return .Continue;
 }
