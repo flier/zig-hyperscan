8f45a1b7e1d2a6a8c0e949af92a7ee2a552cf61d
Flier Lu <flier.lu@gmail.com>
2025-10-12 19:01:29 +0800

    Refactor version struct in Hyperscan to simplify current_version representation
    This commit removes the explicit Version struct and simplifies the current_version definition by directly using a struct literal. This change streamlines the versioning code while maintaining the same functionality in tests.

--- src/version.zig
+++ src/version.zig
@@ -16,33 +16,15 @@ test version {
     try std.testing.expectEqualStrings(version(), hs.HS_VERSION_STRING);
 }
 
-/// A version struct to identify this release of Hyperscan.
-pub const Version = struct {
-    /// A 32-bit version number.
-    value: u32,
-    /// The major version number.
-    major: u32,
-    /// The minor version number.
-    minor: u32,
-    /// The patch version number.
-    patch: u32,
-    /// A version string.
-    string: []const u8,
-};
-
 /// The current Hyperscan version.
-pub const current_version = Version{
-    .value = hs.HS_VERSION_32BIT,
+pub const current_version = .{
     .major = hs.HS_MAJOR,
     .minor = hs.HS_MINOR,
     .patch = hs.HS_PATCH,
-    .string = hs.HS_VERSION_STRING[0..hs.HS_VERSION_STRING.len],
 };
 
 test current_version {
-    try std.testing.expectEqual(current_version.value, hs.HS_VERSION_32BIT);
     try std.testing.expectEqual(current_version.major, hs.HS_MAJOR);
     try std.testing.expectEqual(current_version.minor, hs.HS_MINOR);
     try std.testing.expectEqual(current_version.patch, hs.HS_PATCH);
-    try std.testing.expectEqualStrings(current_version.string, hs.HS_VERSION_STRING[0..hs.HS_VERSION_STRING.len]);
 }
